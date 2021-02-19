c47287680d4927dd53d3521e38a738b19f2044f1
Etuk Mfon <emfon71@gmail.com>
2021-02-19 12:58:54 +0100

    added hero and about sections

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,60 @@
+import style from '@/styles/Home.module.scss';
+import Image from 'next/image';
+
 export default function Home({}) {
     return (
-        <main>
-            <h1>Home</h1>
+        <main className="pageStyle">
+            {/*hero section start*/}
+            <section className={style.heroWrapper}>
+                {/*background image*/}
+                <Image
+                    className={style.heroImage}
+                    src="/assets/images/hero-pattern.jpg"
+                    alt="background image"
+                    layout={'fill'}
+                />
+                {/*gradient overlay*/}
+                <div className={style.gradient} />
+                {/*h2 and paragraph*/}
+                <div className={style.headlineWrapper}>
+                    <h2 className={style.headlineHeader}>Dedicated Medicare</h2>
+                    <p className={style.headlineSubHeader}>
+                        Because your health matters most
+                    </p>
+                </div>
+            </section>
+            {/*hero section end*/}
+
+            {/*about us section start*/}
+            <section className={style.aboutUsSectionWrapper}>
+                {/*about us image||left grid*/}
+                <div className="h-full w-full relative">
+                    <Image
+                        className={style.aboutUsImage}
+                        src={'/assets/images/about-us-bg.png'}
+                        alt="our dedication image"
+                        layout={'fill'}
+                    />
+                </div>
+                {/*about us headline||right grid*/}
+                {/*about us headline wrapper*/}
+                <div className={style.aboutUsHeadlineWrapper}>
+                    {/*about us sub header*/}
+                    <p className={style.aboutUsHeadlineSubHeader}>About Us</p>
+                    {/*about us header*/}
+                    <h2 className={style.aboutUsHeadlineHeader}>
+                        We established cooperation with various global partners.
+                    </h2>
+                    {/*about us headline body*/}
+                    <p className={style.aboutUsHeadlineBody}>
+                        We've combined a new kind of doctor's experience that
+                        blends the highest level of health care with exceptional
+                        service. People can enroll membership by paying only an
+                        annual membership fee of just $129.
+                    </p>
+                </div>
+            </section>
+            {/*about us section end*/}
         </main>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,22 @@ module.exports = {
             screens: {
                 fhd: '1920px'
             },
+            backgroundImage: (theme) => ({
+                'hero-pattern': 'url(/assets/images/hero-pattern.jpg)',
+                'line-bg': 'url(/assets/images/line-bg.png)',
+                'banner-bg': 'url(/assets/images/banner-bg.jpg)'
+            }),
             colors: {
-                'deep-blue': '#3b49df'
+                'deep-blue': '#3b49df',
+                'dark-teal': '#3C8080'
+            },
+            height: {
+                100: '30rem',
+                105: '35rem',
+                110: '40rem'
+            },
+            fontSize: {
+                '4.5xl': '2.75rem'
             },
             borderWidth: {
                 10: '10px'
